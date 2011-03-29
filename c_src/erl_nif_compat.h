92dc48542fd52bb9f8e7c400166f1cc17ecd927b
Filipe David Manana <fdmanana@apache.org>
2011-03-29 18:26:26 +0100

    A more flexible erl_nif_compat.h version
    Ensure that future otp releases get the same compatibility macros as those defined
    for OTP R14A/B (the minor and major version macro values for R14B02 changed).
    Also added a few more compat macros:
    
    * enif_realloc_binary_compat
    * enif_release_binary_compat
    * enif_priv_data_compat
    * enif_make_string_compat
    * enif_make_uint_compat

--- c_src/erl_nif_compat.h
+++ c_src/erl_nif_compat.h
@@ -7,19 +7,57 @@ extern "C" {
 
 #include "erl_nif.h"
 
-#if ERL_NIF_MAJOR_VERSION == 1 && ERL_NIF_MINOR_VERSION == 0
+
+#if ERL_NIF_MAJOR_VERSION == 0 && ERL_NIF_MINOR_VERSION == 1
+#define OTP_R13B03
+#elif ERL_NIF_MAJOR_VERSION == 1 && ERL_NIF_MINOR_VERSION == 0
+#define OTP_R13B04
+#elif ERL_NIF_MAJOR_VERSION == 2 && ERL_NIF_MINOR_VERSION == 0
+#define OTP_R14
+#elif ERL_NIF_MAJOR_VERSION == 2 && ERL_NIF_MINOR_VERSION == 1
+#define OTP_R14B02
+#endif
+
+
+#ifdef OTP_R13B03
 
 #define enif_open_resource_type_compat enif_open_resource_type
 #define enif_alloc_resource_compat enif_alloc_resource
 #define enif_release_resource_compat enif_release_resource
 #define enif_alloc_binary_compat enif_alloc_binary
 #define enif_alloc_compat enif_alloc
+#define enif_release_binary_compat enif_release_binary
 #define enif_free_compat enif_free
 #define enif_get_atom_compat enif_get_atom
+#define enif_priv_data_compat enif_get_data
+#define enif_make_uint_compat enif_make_ulong
+
+#define enif_make_string_compat(E, B, Enc) \
+    enif_make_string(E, B)
+
+#endif /* R13B03 */
+
+
+#ifdef OTP_R13B04
+
+#define enif_open_resource_type_compat enif_open_resource_type
+#define enif_alloc_resource_compat enif_alloc_resource
+#define enif_release_resource_compat enif_release_resource
+#define enif_alloc_binary_compat enif_alloc_binary
+#define enif_realloc_binary_compat enif_realloc_binary
+#define enif_release_binary_compat enif_release_binary
+#define enif_alloc_compat enif_alloc
+#define enif_free_compat enif_free
+#define enif_get_atom_compat enif_get_atom
+#define enif_priv_data_compat enif_priv_data
+#define enif_make_string_compat enif_make_string
+#define enif_make_uint_compat enif_make_uint
 
 #endif /* R13B04 */
 
-#if ERL_NIF_MAJOR_VERSION == 2 && ERL_NIF_MINOR_VERSION == 0
+
+/* OTP R14 and future releases */
+#if !defined(OTP_R13B03) && !defined(OTP_R13B04)
 
 #define enif_open_resource_type_compat(E, N, D, F, T) \
     enif_open_resource_type(E, NULL, N, D, F, T)
@@ -33,6 +71,12 @@ extern "C" {
 #define enif_alloc_binary_compat(E, S, B) \
     enif_alloc_binary(S, B)
 
+#define enif_realloc_binary_compat(E, S, B) \
+    enif_realloc_binary(S, B)
+
+#define enif_release_binary_compat(E, B) \
+    enif_release_binary(B)
+
 #define enif_alloc_compat(E, S) \
     enif_alloc(S)
 
@@ -42,8 +86,11 @@ extern "C" {
 #define enif_get_atom_compat(E, T, B, S) \
     enif_get_atom(E, T, B, S, ERL_NIF_LATIN1)
 
-#endif /* R14 */
+#define enif_priv_data_compat enif_priv_data
+#define enif_make_string_compat enif_make_string
+#define enif_make_uint_compat enif_make_uint
 
+#endif  /* R14 and future releases */
 
 
 #ifdef __cplusplus
